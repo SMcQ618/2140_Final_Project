e55fc8a16390d65d22a8184887172f000777d6ff
Stephen Sodipo <stephensodipo13@gmail.com>
2023-08-07 23:24:00 -0400

    Started working on the more advance topics

--- Advance_methods.py
+++ Advance_methods.py
@@ -1,4 +1,9 @@
-
+import math
+import numpy
+import stat
 
 class Adv_Methods:
-    """Will contain the more complex and advance opperations for calculator"""
+    """Will contain the more complex and advance opperations for calculator. 
+    This includes matricies, more on integration, probability, plotting and graphing, 
+    vectors, stats, probability, financial calculatos"""
+    #notes: should add matrix, 
