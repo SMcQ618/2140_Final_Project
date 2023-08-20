f4ffff69d969734b05aba377c37ddc1e9b9798f2
Stephen Sodipo <stephensodipo13@gmail.com>
2023-08-20 11:41:36 -0400

    making progress with the output

--- project files/DifferentialEquationSolver.py
+++ project files/DifferentialEquationSolver.py
@@ -1,52 +1,60 @@
 import tkinter as tk
 import sympy as sp
-from differential_equations import DifferentialEqs
+import Differential_Equations1
 
 class DifferentialEquationSolver:
     def __init__(self, master):
+        #initialize the gui 
         self.master = master
         self.master.title("Differential Equation Solver")
-
+        #labeling
         self.order_label = tk.Label(master, text="Order:")
-        self.order_label.pack()
+        self.order_label.grid(row = 0, column= 0, sticky= "w")
 
         self.order_entry = tk.Entry(master)
-        self.order_entry.pack()
+        self.order_entry.grid(row = 0, column = 1)
 
         self.variable_label = tk.Label(master, text="Variable:")
-        self.variable_label.pack()
+        self.variable_label.grid(row = 1, column = 0, sticky = "w")
 
         self.variable_entry = tk.Entry(master)
-        self.variable_entry.pack()
+        self.variable_entry.grid(row = 1, column = 1)
 
         self.equation_label = tk.Label(master, text="Equation:")
-        self.equation_label.pack()
+        self.equation_label.grid(row = 2, column = 0, sticky = "w")
 
         self.equation_entry = tk.Entry(master)
-        self.equation_entry.pack()
-
+        self.equation_entry.grid(row = 2, column = 1)
+        #the button to solve the diff eq
         self.solve_button = tk.Button(master, text="Solve", command=self.solve_differential_eq)
-        self.solve_button.pack()
-
+        self.solve_button.grid(row = 3, column = 2)
+        #labeling the display
         self.results_label = tk.Label(master, text="", font=("Helvetica", 12))
-        self.results_label.pack()
+        self.results_label.grid(row = 4, columnspan = 2)
 
         self.close_button = tk.Button(master, text="Close", command=master.destroy)
-        self.close_button.pack()
+        self.close_button.grid(row = 5, columnspan = 2)
 
     def solve_differential_eq(self):
+        """_summary_Solves the diff eq based on the user input"""
         order = int(self.order_entry.get())
         variable_symbol = self.variable_entry.get()
-        #variable = sp.symbols(variable_symbol)
-        equation = self.equation_entry.get()
-
-        diff_eq = DifferentialEqs(order, variable_symbol, equation)
+        variable = sp.Function(variable_symbol)(sp.symbols('x'))
+        equation = sp.simplify(self.equation_entry.get())
+        #calls the diffeq function to solve
+        diff_eq = Differential_Equations1(order, equation, variable)
         general_solution = diff_eq.general_solution()
 
-        self.display_results(general_solution)
+        self.display_results(order, general_solution)
 
-    def display_results(self, result):
-        self.results_label.config(text=f"General Solution:\n{result}")
+    def display_results(self, order, result):
+        """DIsplays the result for the dff eq solver
+
+        Args:
+            order (int): The order of the diff eq
+            result (str): the result of the gen solution
+        """
+        self.results_label.config(text=f"Order: {order} \n General Solution:\n{result}")
 
 root = tk.Tk()
 app = DifferentialEquationSolver(root)
--- project files/Differential_Equations1.py
+++ project files/Differential_Equations1.py
@@ -1,53 +1,113 @@
 import sympy as sp
+from sympy import symbols, Function, Eq, dsolve, Derivative, exp, cos, sin, tan, parse_expr, pprint
+import numpy as np
+from sympy.solvers.ode.systems import dsolve_system
+#redoing the differential equation class
 
-class DifferentialEqs:
-    def __init__(self, equation, function):
-        self.equation = equation
-        self.function = function
-        self.variable = function.args[0]
-        self.solution = None
-        self.is_linear = self.check_linear()
+def solve_ode(eq, var, order):
+    """Solve a sungle ordinary differential equation.
 
-    def check_linear(self):
-        return sp.linear_eq_to_matrix(self.equation.rhs, [self.function.diff()])[0] is not None
+    Args:
+        eq (sympy.Expr): the differential equation
+        var (str): The variable to solve for
+        order (int): the order of the differential equaiton either 1 or 2
 
-    def solve(self):
-        self.solution = sp.dsolve(self.equation, self.function)
-        return self.solution
+    Returns:
+        list: A list of solutions corresponding to the given order
+    """
+    #have the code recognize 't' as a symbol so it doesn't break
+    t = symbols('t')#To create an undefined function, pass a string name to Function
+    y = Function(var)(t)
 
-    def solve_initial_condition(self, initial_conditions):
-        if self.solution is None:
-            self.solve()
-        if not self.is_linear:
-            raise ValueError("Initial condition solving is currently supported only for linear ODEs.")
+    #create a sympy equation representing the given dff eq
+    diff_eq = Eq(Derivative(y, t, order), eq)
 
-        # initial_conditions_f = {
-        #     self.function: initial_conditions[self.function],
-        #     self.function.diff(): initial_conditions[self.function.diff()]}
-        
-        constants = sp.solve([self.solution.rhs.subs(initial_conditions),
-                              self.solution.lhs.subs(initial_conditions)])
-        particular_solution = self.solution.subs(constants)
-        return particular_solution
+    if order == 1:
+        #for first order use the dsolve direction
+        solution = dsolve(diff_eq)
+        #dolve is a sympy function that solves ordinary equations
+        #returns an object that represents the gen solution with C1,C2 like in class
+    else:
+        #second or higher converts to a system to solve
+        system_eq = [diff_eq.subs(Derivative(t, y, 1), symbols(f'{var}^{i}'))for i in range(order)]
+        system = dsolve_system(system_eq, funcs=[y])
+        #dsolve_system is another method to solve
+        solution = [eq.rhs for eq in system[0]]
 
-    def general_solution(self):
-        if not self.solution:
-            self.solve()
-        return self.solution
+    return solution
 
-# Example usage
-if __name__ == "__main__":
-    x = sp.symbols('x')
-    f = sp.Function('f')(x)
-    eq = sp.Eq(x * f.diff(x) + 2 * f, 0)
+def is_linear(eq, var):
+    """Checks if a inputted equation is linear
 
-    diff_eq_solver = DifferentialEqs(eq, f)
+    Args:
+        eq (sympy.Expr): The differential equation
+        var (str): the variable to check against
+
+    Returns:
+        bool: Returns true if the equation is linear, false otherwise
+    """
+    t = symbols('t')
+    y = Function(var)(t)
+    for term in eq.args:
+        if term.has(Derivative(y, t)):
+            if term.coeff(Derivative(y, t)) != 0:
+                return False
+            for var_pow in term.as_poly().terms():
+                if var_pow[1] > 1:
+                    return False
+        elif term.has(y):
+            if term.coeff(y) != 0:
+                return False
+    return True
+
+#test to check
+def main():
+    print('Differential Equation solver\n')
     
-    solution = diff_eq_solver.solve()
-    print("General solution:", solution)
+    order = 1
+    var = "y"
+
+    t = symbols('t')
+    y = Function(var)(t)
+    #eq = parse_expr(eq_str)
+
+    eq = Eq(Derivative(y, t, order) + 2*y, 0)
     
-    print("Is linear:", diff_eq_solver.is_linear)
+    if is_linear(eq, var):
+        print('The differential equation is linear.')
+    else:
+        print('The differential equation is not linear.')
+
+    print("Equation:")
+    pprint(eq, use_unicode=True)
     
-    initial_condition = {f: sp.Function('f')(0), f.diff(): 1}  # Adjust initial conditions
-    particular_solution = diff_eq_solver.solve_initial_condition(initial_condition)
-    print("Particular solution with initial conditions:", particular_solution)
+    solutions = solve_ode(eq, var, order)
+    print("\nSolutions:")
+    for solution in solutions:
+        pprint(solution, use_unicode=True)
+
+if __name__ == '__main__':
+    main()
+
+'''def main():
+    print('Differential Equation solver\n')
+    
+    eq_str = input('Enter the differential equation: ')
+    order = int(input('What order is the equation (must be 1st or 2nd): '))
+    var = input('Enter the variable to solve for: ')
+
+    t = symbols('t')
+    y = Function(var)(t)
+    eq = parse_expr(eq_str)
+
+    if is_linear(eq, var):
+        print('The differential equation is linear.')
+    else:
+        print('The differential equation is not linear.')
+
+    pprint(eq, use_unicode = True)
+    pprint(solve_ode(eq, var, order), use_unicode = True)
+
+if __name__ == '__main__':
+    main()'''
+
