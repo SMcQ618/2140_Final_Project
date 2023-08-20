f2210b5fe25797fc1b434c296f06fd9d8f9bd96f
Stephen Sodipo <stephensodipo13@gmail.com>
2023-08-20 16:16:13 -0400

    trying to change some stuff

--- project files/DifferentialEquationSolver.py
+++ project files/DifferentialEquationSolver.py
@@ -1,51 +1,51 @@
 import tkinter as tk
-import sympy as sp
+from sympy import Function, Eq, Derivative, dsolve, pprint
+from sympy.parsing.sympy_parser import (parse_expr, standard_transformations, implicit_multiplication_application)
 import Differential_Equations1
 
 class DifferentialEquationSolver:
-    def __init__(self, master):
+    def __init__(self, root):
         #initialize the gui 
-        self.master = master
-        self.master.title("Differential Equation Solver")
-        #labeling
-        self.order_label = tk.Label(master, text="Order:")
-        self.order_label.grid(row = 0, column= 0, sticky= "w")
+        self.root = root
+        self.root.title('Dfferential Equation Solver /n')
+        
+        self.order_label = tk.Label(root, text = 'Order')
+        self.order_label.pack()
 
-        self.order_entry = tk.Entry(master)
-        self.order_entry.grid(row = 0, column = 1)
+        self.order_entry = tk.Entry()
+        self.order_entry.pack()
 
-        self.variable_label = tk.Label(master, text="Variable:")
-        self.variable_label.grid(row = 1, column = 0, sticky = "w")
+        self.variable_label = tk.Label(root, text = 'What Variable do you want to solve')
+        self.variable_label.pack()
 
-        self.variable_entry = tk.Entry(master)
-        self.variable_entry.grid(row = 1, column = 1)
+        self.variable_entry = tk.Entry()
+        self.variable_entry.pack()
 
-        self.equation_label = tk.Label(master, text="Equation:")
-        self.equation_label.grid(row = 2, column = 0, sticky = "w")
+        self.equation_label = tk.Label(root, text = 'Equation: ')
+        self.equation_label.pack()
 
-        self.equation_entry = tk.Entry(master)
-        self.equation_entry.grid(row = 2, column = 1)
-        #the button to solve the diff eq
-        self.solve_button = tk.Button(master, text="Solve", command=self.solve_differential_eq)
-        self.solve_button.grid(row = 3, column = 2)
-        #labeling the display
-        self.results_label = tk.Label(master, text="", font=("Helvetica", 12))
-        self.results_label.grid(row = 4, columnspan = 2)
+        self.equation_entry = tk.Entry()
+        self.equation_entry.pack()
 
-        self.close_button = tk.Button(master, text="Close", command=master.destroy)
-        self.close_button.grid(row = 5, columnspan = 2)
+        self.solve_button = tk.Button(root, text='Solve', command = self.solve_equation)
+        self.solve_button.pack()
+
+        self.answer_label = tk.Label(root, text = 'Answer:')
+        self.answer_label.pack()
+
+        self.answer_text = tk.Text(root, height = 7, width = 40)
+        self.answer_text.pack()
 
     def solve_differential_eq(self):
         """_summary_Solves the diff eq based on the user input"""
         order = int(self.order_entry.get())
-        variable_symbol = self.variable_entry.get()
-        variable = sp.Function(variable_symbol)(sp.symbols('x'))
-        equation = sp.simplify(self.equation_entry.get())
-        #calls the diffeq function to solve
-        diff_eq = Differential_Equations1(order, equation, variable)
-        general_solution = diff_eq.general_solution()
+        variable = self.variable_entry.get()
+        equation_str = self.equation_entry.get()
 
-        self.display_results(order, general_solution)
+        #call the dsolve the func 
+        result = sp.dsolve(equation, variable)
+
+        self.display_results(order, result)
 
     def display_results(self, order, result):
         """DIsplays the result for the dff eq solver
@@ -54,9 +54,12 @@ class DifferentialEquationSolver:
             order (int): The order of the diff eq
             result (str): the result of the gen solution
         """
-        self.results_label.config(text=f"Order: {order} \n General Solution:\n{result}")
+        self.results_label.config(text=f"Order: {order} \nGeneral Solution:\n{result}")
 
-root = tk.Tk()
-app = DifferentialEquationSolver(root)
+def main():
+    root = tk.Tk()
+    app = DifferentialEquationSolver(root)
+    root.mainloop()
 
-root.mainloop()
+if __name__ == "__main__":
+    main()
